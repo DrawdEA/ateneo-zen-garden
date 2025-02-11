764d85e21eb92b99e9d8d525f022cdbff73b6839
Edward Joshua Diesta <99942301+DrawdEA@users.noreply.github.com>
2025-02-11 15:34:45 +0800

    Initialize SceneFrame.java

--- SceneFrame.java
+++ SceneFrame.java
@@ -1,5 +1,28 @@
+import java.awt.*;
+import java.util.*;
+import javax.swing.*;
+
 public class SceneFrame {
-    public void SetUpGUI() {
-        
+    JFrame mainFrame;
+
+    /**
+     * Instantiates a new frame.
+     */
+    public SceneFrame() {
+        mainFrame = new JFrame();
+    }
+    
+    /**
+     * Sets up the GUI.
+     */
+    public void setUpGUI() {
+        mainFrame.setTitle("Midterm Project - Diesta - Uy, C.");
+    }
+
+    /**
+     * Sets up the button listeners for the actions.
+     */
+    public void setUpButtonListeners() {
+
     }
 }
