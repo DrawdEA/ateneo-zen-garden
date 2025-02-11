b1263a38b6d59b05557d659b43bd3594b3f685ac
Edward Joshua Diesta <99942301+DrawdEA@users.noreply.github.com>
2025-02-11 16:34:17 +0800

    Initialize GUI setup

--- SceneCanvas.java
+++ SceneCanvas.java
@@ -10,6 +10,25 @@ public class SceneCanvas extends JComponent {
      */
     public SceneCanvas() {
         drawingObjects = new ArrayList<DrawingObject>();
+
+        // Add the DrawingObjects in the array list.
+        drawingObjects.add(new Circle()); // Sample shape
+        drawingObjects.add(new Line()); // Sample shape
+        drawingObjects.add(new Square()); // Sample shape
+
+        // Set up miscellaneous details.
         this.setPreferredSize(new Dimension(800, 600));
+    }
+
+    /**
+     * Draws every shape in the drawingObjects array list.
+     * 
+     * @param g main graphics object
+     */
+    @Override
+    protected void paintComponent(Graphics g) {
+        for (DrawingObject object : drawingObjects) {
+            object.draw((Graphics2D) g);
+        }
     }
 }
--- SceneFrame.java
+++ SceneFrame.java
@@ -5,18 +5,34 @@ import javax.swing.*;
 public class SceneFrame {
     JFrame mainFrame;
 
+    SceneCanvas canvas;
+    JButton actionButton;
+
     /**
      * Instantiates a new frame.
      */
     public SceneFrame() {
         mainFrame = new JFrame();
+
+        canvas = new SceneCanvas();
+        actionButton = new JButton("Some Action Here");
     }
     
     /**
      * Sets up the GUI.
      */
     public void setUpGUI() {
+        // Set up the GUI hierarchy.
+        mainFrame.add(canvas, BorderLayout.CENTER);
+        mainFrame.add(actionButton, BorderLayout.SOUTH);
+
+        // Set up the miscellaneous details.
         mainFrame.setTitle("Midterm Project - Diesta - Uy, C.");
+        mainFrame.setResizable(false);
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.setVisible(true);
+
+        // Pack the frame so that the frame will size itself based on its contents.
         mainFrame.pack();
     }
 
