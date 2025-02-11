0eb41f6b1227215729c4639568fb7181f717bf0d
Edward Joshua Diesta <99942301+DrawdEA@users.noreply.github.com>
2025-02-11 15:38:12 +0800

    Initialize SceneStarter.java

---
+++ SceneStarter.java
@@ -0,0 +1,7 @@
+public class SceneStarter {
+    public static void main(String args[]) {
+        SceneFrame canvas = new SceneFrame();
+        canvas.setUpGUI();
+        canvas.setUpButtonListeners();
+    }
+}
