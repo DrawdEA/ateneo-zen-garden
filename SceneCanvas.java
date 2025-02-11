ddfd8236f7c72318ff7f9af671aef9cfa3dfb144
Edward Joshua Diesta <99942301+DrawdEA@users.noreply.github.com>
2025-02-11 15:54:01 +0800

    Add constructor

--- SceneCanvas.java
+++ SceneCanvas.java
@@ -4,16 +4,12 @@ import javax.swing.*;
 
 public class SceneCanvas extends JComponent {
     ArrayList<DrawingObject> drawingObjects; 
-    
+
     /**
-     * 
+     * Instantiate a SceneCanvas (an extension of JComponent).
      */
     public SceneCanvas() {
         drawingObjects = new ArrayList<DrawingObject>();
         this.setPreferredSize(new Dimension(800, 600));
-    }
-
-    public void paint(Graphics g) {
-
     }
 }
