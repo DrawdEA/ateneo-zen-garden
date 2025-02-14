d175e67151f4b23a422492bda9508094a1a47eb7
Charles Joshua Uy <75460325+CJ-Uy@users.noreply.github.com>
2025-02-14 13:40:41 +0800

    Almost there at laptop folding

--- README.md
+++ README.md
@@ -2,7 +2,14 @@ REFERENCES HERE:
 
 Tutorials:
 https://www.youtube.com/watch?v=KcEvHq8Pqs0&ab_channel=BroCode
+https://www.youtube.com/watch?v=tHNWIWxRDDA
 https://youtu.be/AheaTd_l5Is?si=Ok_50NX6FGO1d87X
+https://zetcode.com/gfx/java2d/transformations/
+https://docs.oracle.com/javase/8/docs/api/java/awt/geom/AffineTransform.html
+https://www.geeksforgeeks.org/thread-sleep-method-in-java-with-examples/
+https://www.javaguides.net/2023/08/interruptedexception-in-java.html
+https://www.geeksforgeeks.org/how-to-return-an-array-in-java/
+https://medium.com/@junfeng142857/affine-transformation-why-3d-matrix-for-a-2d-transformation-8922b08bce75
 
 Fonts:
 https://fonts.google.com/specimen/IBM+Plex+Mono
---
+++ test/Laptop.java
@@ -0,0 +1,70 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.geom.AffineTransform;
+import javax.swing.*;
+
+public class Laptop extends JFrame{
+
+    public Laptop(){
+        super("Laptop");
+        
+        //-----Frame setup-----//
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(new BorderLayout());
+        this.setResizable(false);
+        this.setSize(800,600);
+        this.setLocation(400,0);
+        this.setVisible(true);
+    }
+
+    public class DrawOpeningLaptop extends JPanel implements ActionListener{
+        public Timer timer;
+        public int laptopDegrees; 
+        public Graphics2D g2d;
+
+        public DrawOpeningLaptop() {
+            super.setDoubleBuffered(true);
+            setFocusable(true);
+            setFocusTraversalKeysEnabled(false);
+
+            laptopDegrees = 0;
+            timer = new Timer(10, this);
+            timer.start();
+        }
+
+        @Override
+        public void paint(Graphics g) {
+            super.paintComponent(g);
+            g2d = (Graphics2D) g.create();
+            g2d.setPaint(Color.green);
+                     
+            AffineTransform fold = new AffineTransform();
+
+            fold.setToScale(1, Math.cos(Math.toRadians(laptopDegrees)));
+            fold.translate(0, 300 * Math.sin(Math.toRadians(laptopDegrees)));
+            
+            g2d.setTransform(fold);
+            g2d.fillRect(150, 400, 500, 300);  
+
+            g2d.dispose();
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent ae){
+            if (laptopDegrees < 180) {
+                laptopDegrees++;
+            }
+            repaint();
+        }
+    }
+
+    public void open(){
+        this.add(new DrawOpeningLaptop(), BorderLayout.CENTER);
+    }
+
+    public static void main(String[] args) {
+        Laptop console = new Laptop();
+        console.open();
+    }
+}
